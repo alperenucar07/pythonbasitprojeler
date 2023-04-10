e2db347610ab1d8060b66b437368cea41de61d5c
alperenucar07 <129285205+alperenucar07@users.noreply.github.com>
2023-04-10 21:28:48 +0300

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.shape("circle")
+turtle.begin_fill()
+turtle.color("white","black")
+turtle.fd(100)
+turtle.left(120)
+turtle.fd(100)
+turtle.left(120)
+turtle.fd(100)
+turtle.end_fill()
